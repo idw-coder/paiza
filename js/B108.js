c1f15014fa0beafc7ab4a197ead41f47f2e22c76
ida-p <i21155388@gmail.com>
2025-10-09 08:36:16 +0900

    refactor: improve calc function by enhancing array handling and adding debug logs

--- js/B108.js
+++ js/B108.js
@@ -2,36 +2,65 @@ function calc(n, m, array1, array2) {
   // array1を2次元配列に変換
   let array12D = [];
   for (let i = 0; i < n; i++) {
-    array12D.push(array1[i].split('').map(Number));
+    array12D.push(array1[i].split("").map(Number));
   }
-  console.log(array12D);
+  // console.log(array12D);
 
   let currentIndex = 0;
   for (let i = 0; i < m; i++) {
     if (array12D[currentIndex % array12D.length][0] >= array2[i]) {
       array12D[currentIndex % array12D.length].push(parseInt(array2[i]));
       currentIndex++;
-      console.log("収まった",array12D[currentIndex % array12D.length][0]);
+      // console.log("array12D[currentIndex % array12D.length][0]", array12D[currentIndex % array12D.length][0]);
+      // console.log("array2[i]", i, array2[i]);
+      // console.log("収まった", array12D[currentIndex % array12D.length][0]);
     } else {
-      array12D[currentIndex % array12D.length].push(parseInt(array12D[currentIndex % array12D.length][0]));
+      // console.log("else", i, array2[i]);
+      array12D[currentIndex % array12D.length].push(
+        parseInt(array12D[currentIndex % array12D.length][0])
+      );
+      // console.log("i", i);
+      // console.log("array12D", array12D);
       currentIndex++;
-      while (array12D[currentIndex % array12D.length][0] < (array2[i] - array12D[(currentIndex - 1) % array12D.length][0])) {
-        array12D[currentIndex % array12D.length].push(parseInt(array2[i] - array12D[(currentIndex - 1) % array12D.length][0]));
+      while (
+        array12D[currentIndex % array12D.length][0] <
+        array2[i] - array12D[(currentIndex - 1) % array12D.length][0]
+      ) {
+        array12D[currentIndex % array12D.length].push(
+          parseInt(
+            array2[i] - array12D[(currentIndex - 1) % array12D.length][0]
+          )
+        );
         //もともとの値と、引き算した値を出力
-        console.log("もともとの値",array12D[currentIndex % array12D.length][0]);
-        console.log("引き算した値",array12D[(currentIndex - 1) % array12D.length][0]);
+        // console.log(
+        //   "もともとの値",
+        //   array12D[currentIndex % array12D.length][0]
+        // );
+        // console.log(
+        //   "引き算した値",
+        //   array12D[(currentIndex - 1) % array12D.length][0]
+        // );
         currentIndex++;
       }
-      array12D[currentIndex % array12D.length].push(parseInt(array2[i]));
+      array12D[currentIndex % array12D.length].push(parseInt(
+        array2[i] - array12D[(currentIndex - 1) % array12D.length][0]
+      ));
+      // console.log("array2[i]", i, array2[i]);
+      // console.log("array12D[(currentIndex - 1) % array12D.length][0]", array12D[(currentIndex - 1) % array12D.length][0]);
+      // console.log("引き算した値", array2[i] - array12D[(currentIndex - 1) % array12D.length][0]);
+      // console.log("収まった", array12D[currentIndex % array12D.length][0]);
+      // console.log("array12D", array12D);
       currentIndex++;
     }
   }
 
-  return array12D;
+  console.log("最終的なarray12D:", array12D);
+  for (let i = 0; i < array12D.length; i++) {
+    console.log(array12D[i].slice(1).reduce((prev, next) => prev + next, 0));
+  }
 }
 
-
-module.exports = {calc};
+module.exports = { calc };
 
 // テスト時は標準入力処理をスキップ
 if (require.main === module) {
@@ -48,8 +77,8 @@ if (require.main === module) {
   });
 
   reader.on("close", () => {
-    const N = parseInt(lines[0].split(' ')[0]);
-    const M = parseInt(lines[0].split(' ')[1]);
+    const N = parseInt(lines[0].split(" ")[0]);
+    const M = parseInt(lines[0].split(" ")[1]);
 
     let inputArray1 = [];
     let inputArray2 = [];
@@ -57,13 +86,11 @@ if (require.main === module) {
       inputArray1.push(lines[i]);
     }
     for (let i = N + 1; i < N + M + 1; i++) {
-      inputArray2.push(lines[i]);
+      inputArray2.push(parseInt(lines[i]));
     }
 
     // console.log(inputArray1);
-    // console.log(inputArray2);
-
-    const result = calc(N, M, inputArray1, inputArray2);
-    console.log(result);
+    // console.log("inputArray2", inputArray
+    calc(N, M, inputArray1, inputArray2);
   });
 }
