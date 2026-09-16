242d8d8529f8c7c03f16f6fc4a607a7690bbc599
ida-work <i21155388@gmail.com>
2026-09-16 19:51:17 +0900

    feat: 入力ファイルを新しいデータエントリで更新し、不要なTypeScriptファイルとマップファイルを削除

---
+++ ts/src/paiza/260915_B84.ts
@@ -0,0 +1,66 @@
+import readline from 'readline'
+const DEBUG = !!process.env.DEBUG
+function solve(N: number, M: number, K: number, myList: number[], t: number[][]) {
+  if (DEBUG)
+    console.log(
+      '\n\n' +
+        N +
+        ' ' +
+        M +
+        ' ' +
+        K +
+        '\n' +
+        myList.join(' ') +
+        '\n' +
+        t.map((c) => c.join(' ')).join('\n')
+    )
+  const result: number[] = []
+  /**
+   * お店の評価を星 4 段階で登録することができます。行ったことのないお店は星 0 で、
+   * 行ったことのあるお店は自分の評価によって星 1 から 3 の間で評価することができます。
+   * あるユーザーに対し、自分とそのユーザーの両方とも評価が星 3 であるようなお店の数がある定数 K 以上であるとき、
+   * 「好みが似ているユーザー」と定義します。
+   * N 店の中から、自分が行ったことがなく、かつ「好みが似ているユーザー」が星 3 の評価をしているようなお店を全て出力
+   * なければ no
+   */
+
+  const similarList = t.filter((shops) => {
+    return (
+      K <= shops.reduce((count, value, i) => count + (value === 3 && myList[i] === 3 ? 1 : 0), 0)
+    )
+  })
+  if (DEBUG) console.log(similarList.join('\n'))
+
+  for (let i = 0; i < N; i++) {
+    if (myList[i] === 0) {
+      for (const similar of similarList) {
+        if (similar[i] === 3) {
+          result.push(i + 1)
+          break
+        }
+      }
+    }
+  }
+
+  console.log(result.length ? result.join(' ') : 'no')
+}
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+const lines: string[] = []
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+reader.on('line', (line: string) => {
+  lines.push(line)
+})
+
+reader.on('close', () => {
+  const [N, M, K] = lines[0]!.split(' ').map(Number) as [number, number, number]
+  const s = lines[1]!.split(' ').map(Number)
+  const t = Array.from({ length: M }, (_, idx) => lines[idx + 2]!.split(' ').map(Number))
+
+  solve(N, M, K, s, t)
+})
